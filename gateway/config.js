564d41843df93255c5378a2bbba8483de1b6f7b1
XiangcaiHuang <HuangXiangcai666@163.com>
2017-08-13 12:48:24 +0800

    app: simulated nodes debug OK

--- gateway/config.js
+++ gateway/config.js
@@ -5,7 +5,7 @@ LICENSE
  * \date    2017-8-12
  * \author  Xiangcai Huang
  * \brief   configuration of OpenThread 
-    Application Gateway components.
+		Application Gateway components.
 --------------------------------------------- */
 var config = {}
 
@@ -15,48 +15,50 @@ var config = {}
 // Gateway's IPv6 address:
 //      fdde:ba7a:b1e5:0:35d1:c886:ea1e:8bb3
 
-// Node command:
-//      coap put fdde:ba7a:b1e5:0:35d1:c886:ea1e:8bb3 lock_btn con 1
-//      coap put fdde:ba7a:b1e5:0:35d1:c886:ea1e:8bb3 light_btn con 2
+// Commands in simulated Nodes:
+//      coap put fdde:ba7a:b1e5:0:35d1:c886:ea1e:8bb3 lock_sta con 1
+
+//      coap put fdde:ba7a:b1e5:0:35d1:c886:ea1e:8bb3 light_sta con 0
+//      coap put fdde:ba7a:b1e5:0:35d1:c886:ea1e:8bb3 temp con 25
 
 config.coap = {
-        gwAddr:         'fdde:ba7a:b1e5:0:35d1:c886:ea1e:8bb3' // Gateway's IPv6 address
-      , frontdoorAddr:  'fdde:ba7a:b1e5:0:d7a3:1de2:916f:8d50' // Frontdoor's IPv6 address
-      , livingroomAddr: 'fdde:ba7a:b1e5:0:31a2:d832:9c48:59d5' // Livingroom's IPv6 address
-      , localAddr: '::1' // Localhost's IPv6 address
-      , nodePort:  5684
-      , gwPort:    5683
-      , nodeFrontdoor:  'frontdoor'
-      , nodeLivingroom: 'livingroom'
-      , lockSta:  'lock_sta'
-      , lightSta: 'light_sta'
-      , temp:     'temp'
-      , valOn:      '1'
-      , valOff:     '0'
-      , valDefault: '0'
+	  gwAddr:         'fdde:ba7a:b1e5:0:73e9:923:a21d:e66f' // Gateway's IPv6 address
+	, frontdoorAddr:  'fdde:ba7a:b1e5:0:a5c9:5611:c34a:b41a' // Frontdoor's IPv6 address
+	, livingroomAddr: 'fdde:ba7a:b1e5:0:a861:c2b5:5e7f:9d5' // Livingroom's IPv6 address
+	, localAddr:      '::1' // Localhost's IPv6 address
+	, nodePort:  5683 // use different port like 5684 if you are trying virtual nodes (NodeJs: vr_nodes.js)
+	, gwPort:    5683
+	, nodeFrontdoor:  'frontdoor'
+	, nodeLivingroom: 'livingroom'
+	, lockSta:  'lock_sta'
+	, lightSta: 'light_sta'
+	, temp:     'temp'
+	, valOn:      '1'
+	, valOff:     '0'
+	, valDefault: '0'
 }
 
 // Configuration of the Object Id
 //--------------------------------------------------
 config.ObjectId = {
-        oIdLight: '3311'
-      , oIdTemp:  '3303'
-      , iId:      '0'
-      , rIdLight: '5850'
-      , rIdTemp:  '5700'
+	  oIdLight: '3311'
+	, oIdTemp:  '3303'
+	, iId:      '0'
+	, rIdLight: '5850'
+	, rIdTemp:  '5700'
 }
 
 // Configuration of the http server
 //--------------------------------------------------
 config.httpServer = {
-        port:   80
-      , UIPath: '.././freeboard'
+	  port:   80
+	, UIPath: '.././freeboard'
 }
 
 // Configuration of the web socket server
 //--------------------------------------------------
 config.WebSocketServer = {
-        port: 3001
+	port: 3001
 }
 
 module.exports = config
